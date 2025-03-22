72703b61d0d26f2b13eedfcfd041d4b0ed65ebf4
Muzaffar Ali <muzafark834@gmail.com>
2025-03-22 13:24:24 +0500

    add more projects

--- Assignments 00 to 05/00_intro_python/how_old_are_they.py
+++ Assignments 00 to 05/00_intro_python/how_old_are_they.py
@@ -1,15 +1,15 @@
-# Ali, Bilal, Chand, Danish, aur Ehsan dost hain. Unki age ye hain:
+# Ali, Bilal, Chand, Danish, aur Ehsan dost hain.
 def main():
-    ali = 21
-    bilal = ali + 6
-    chand = bilal + 20
-    danish = chand + ali
-    ehsan = chand 
+   ali = 21
+   bilal = 6 + ali
+   chand = 20 + bilal
+   danish = chand + ali
+   ehsan = chand
 
-    print(f"Ali is {ali} years old")
-    print(f"Bilal is {bilal} years old")
-    print(f"Chand is {chand} years old")
-    print(f"Danish is {danish} years old")
-    print(f"Ehsan is {ehsan} years old")
+   print(f" Ali is {ali}")
+   print(f"Bilal is {bilal}")
+   print(f"Chand is {chand}")
+   print(f"Danish is {danish}")
+   print(f"Ehsan is {ehsan}")
 
 main()
